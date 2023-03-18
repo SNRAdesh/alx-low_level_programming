c334bd2668b5cdb9355d0372be7a26b0991da030
SNRAdesh <adesinatoheeb7@gmail.com>
2023-03-18 07:02:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Aprogram that prints a standard erro 
- * Return:1
+ * main - A c program that prints a line to the standard erro 
+ * Return 1
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
