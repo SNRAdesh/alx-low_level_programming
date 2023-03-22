f91390b6c18a63af13369be122f068ef4cfdd50f
SNRAdesh <adesinatoheeb7@gmail.com>
2023-03-22 01:55:42 +0100

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * _putchar: prints the alphabets
+ * Return: Always 0.
+ */
+int main(void)
+  {
+    int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int t, u;
+
+	u = sizeof(str) / sizeof(int);
+	for (t = 0; t < u; t++)
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
+    return (0);
